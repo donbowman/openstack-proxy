eb399b1fd5029e78e31ab480bccbdf2693cb2e16
don <don.waterloo@gmail.com>
2014-08-22 13:04:38 +0000

    added retry logic on connection to neutron/nova. in particular, the osapi workers can be consumed and we were getting a connection failure

--- sstp-proxy.py
+++ sstp-proxy.py
@@ -24,7 +24,15 @@ import ConfigParser
 import syslog
 import ctypes
 import prctl
+import os
+from time import sleep
 
+
+def log(severity,txt):
+    if (os.isatty(1)):
+        print(txt)
+    else:
+        syslog.syslog(severity,txt)
 
 class NS:
     ns_fd = ""
@@ -45,55 +53,71 @@ class NS:
 
 # expects /path/sra_/tenant/user/instance 
 def find_host(s,admin_user,admin_password,keystone_url):
-    h = "unknown"
+    h = ""
     ns_id = ""
     path = s.split('/')
     if len(path) < 4:
-        syslog.syslog(syslog.LOG_ERR,"sstp-proxy Error: not passed enough arguments (s = '%s')" % s)
+        log(syslog.LOG_ERR,"sstp-proxy Error: not passed enough arguments (s = '%s')" % s)
         return (h,ns_id)
 
     user = path[1]
     tenant = path[2]
     instance = path[3]
 
-    syslog.syslog(syslog.LOG_INFO,"sstp-proxy Use user:%s, tenant:%s, instance=%s" % (user,tenant,instance))
+    log(syslog.LOG_INFO,"sstp-proxy Use user:%s, tenant:%s, instance=%s" % (user,tenant,instance))
 
-    neutron_cl = neutronclient.Client(username=admin_user,
-                       password=admin_password,
-                       tenant_name=tenant,
-                       auth_url=keystone_url)
+    # Sometimes we are getting a 'no connection' to
+    # nova api, added retry logic with exponential backoff
+    for retry in range(0,3):
+        try:
+            neutron_cl = neutronclient.Client(username=admin_user,
+                               password=admin_password,
+                               tenant_name=tenant,
+                               auth_url=keystone_url)
 
 
-    nova_cl = novaclient.client.Client(3,
-                       admin_user,
-                       admin_password,
-                       tenant,
-                       keystone_url)
+            nova_cl = novaclient.client.Client(3,
+                               admin_user,
+                               admin_password,
+                               tenant,
+                               keystone_url,
+                               timeout=3)
 
-    servers = nova_cl.servers.list()
-    routers = neutron_cl.list_routers()
+            servers = nova_cl.servers.list()
+            routers = neutron_cl.list_routers()
 
-    for s in servers:
-        if s.name == instance:
-            for i in s.networks:
-                if (len(s.networks[i])):
-                    h = str(s.networks[i][0])
-                    # how to find ns_id for router on net?
-                    net = neutron_cl.list_networks(name=i)
-                    snet = net['networks'][0]['subnets'][0]
-                    ports = neutron_cl.list_ports(device_owner='network:router_interface')
-                    for p in ports['ports']:
-                        tsn = p['fixed_ips'][0]['subnet_id']    
-                        if (tsn == snet):
-                            ns_id = p['device_id']
-                            break
+            for s in servers:
+                if s.name == instance:
+                    for i in s.networks:
+                        if (len(s.networks[i])):
+                            h = str(s.networks[i][0])
+                            # how to find ns_id for router on net?
+                            net = neutron_cl.list_networks(name=i)
+                            snet = net['networks'][0]['subnets'][0]
+                            ports = neutron_cl.list_ports(device_owner='network:router_interface')
+                            for p in ports['ports']:
+                                tsn = p['fixed_ips'][0]['subnet_id']    
+                                if (tsn == snet):
+                                    ns_id = p['device_id']
+                                    break
+                    break
+            if (h==""):
+                log(syslog.LOG_ERR,"sstp-proxy Error: host %s not found" % instance)
+            if (ns_id == ""):
+                log(syslog.LOG_ERR,"sstp-proxy Error: namespace not found for instance %s" % instance)
+            return (h,ns_id)
+        except ConnectionError:
+            traceback.print_exc()
+            log(syslog.LOG_ERR,"sstp-proxy Error: Exception contacting neutron/nova (retry=%d)" % retry)
+            sleep(1 * retry*retry)
+        except:
+            traceback.print_exc()
+            log(syslog.LOG_ERR,"sstp-proxy Error: Misc Exception contacting neutron/nova")
             break
 
-    if (h==""):
-        syslog.syslog(syslog.LOG_ERR,"sstp-proxy Error: host %s not found" % instance)
-    if (ns_id == ""):
-        syslog.syslog(syslog.LOG_ERR,"sstp-proxy Error: namespace not found for instance %s" % instance)
+    log(syslog.LOG_ERR,"sstp-proxy Error: host %s not found (exhausted retries)" % instance)
     return (h,ns_id)
+
 
 def rforward(source, dest):
     while True:
@@ -129,21 +153,21 @@ def forward(source,admin_user,admin_password,keystone_url):
                                  admin_password,
                                  keystone_url)
                 ibuf = re.sub("^SSTP_DUPLEX_POST.*/sra_","SSTP_DUPLEX_POST /sra_", ibuf)
-            else:
-                if ibuf.startswith('S') != True:
-                    h = "localhost"
-            if (h != "" and ns != ""):
-                syslog.syslog(syslog.LOG_INFO,"Connect SSTP proxy to %s:%d (ns=%s)" % (h,p,ns))
-                try:
-                    _ns = NS(ns)
-                    dest = eventlet.wrap_ssl(eventlet.connect((h,p)),
-                                           cert_reqs=ssl.CERT_NONE
-                                          )
-                    eventlet.spawn_n(rforward, dest, source)
-                except:
+                if (h != "" and ns != ""):
+                    log(syslog.LOG_INFO,"Connect SSTP proxy to %s:%d (ns=%s)" % (h,p,ns))
+                    try:
+                        _ns = NS(ns)
+                        dest = eventlet.wrap_ssl(eventlet.connect((h,p)),
+                                               cert_reqs=ssl.CERT_NONE
+                                              )
+                        eventlet.spawn_n(rforward, dest, source)
+                    except:
+                        source.close()
+                        break
+                    d = ibuf
+                else:
                     source.close()
                     break
-                d = ibuf
         if dest:
             try:
                 dest.sendall(d)
@@ -198,6 +222,6 @@ prctl.cap_effective.sys_admin = True
 
 while True:
     xcl, addr = listener.accept()
-    syslog.syslog(syslog.LOG_INFO, "sstp-proxy accepted connection %s %s" % (xcl, addr))
+    log(syslog.LOG_INFO, "sstp-proxy accepted connection %s %s" % (xcl, addr))
     eventlet.spawn_n(forward, xcl,args.admin_user,args.admin_pass,args.keystone_url)
 
